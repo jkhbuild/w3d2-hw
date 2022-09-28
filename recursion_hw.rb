cdefe3bb7f23589f028d8448e8346e9dc10dc09a
jkhbuild <jkh.build@gmail.com>
2022-09-27 21:27:13 -0400

    completed hw

---
+++ recursion_hw.rb
@@ -0,0 +1,62 @@
+
+def sum_to(n)
+    return 1 if n == 1
+    return nil if !n.positive?
+
+    n + sum_to(n-1)
+end
+# p sum_to(5)  # => returns 15
+# p sum_to(1)  # => returns 1
+# p sum_to(9)  # => returns 45
+# p sum_to(-8)  # => returns nil
+
+def add_numbers(arr_nums)
+    return nil if arr_nums.length == 0
+    return arr_nums[0] if arr_nums.length == 1
+
+    arr_nums.shift + add_numbers(arr_nums)
+end
+
+# p add_numbers([1,2,3,4]) # => returns 10
+# p add_numbers([3]) # => returns 3
+# p add_numbers([-80,34,7]) # => returns -39
+# p add_numbers([]) # => returns nil
+
+def gamma_fnc(n)
+    return nil if n == 0
+    return 1 if n == 1
+
+    
+    (n-1) * gamma_fnc(n-1)
+end
+# p gamma_fnc(0)  # => returns nil
+# p gamma_fnc(1)  # => returns 1
+# p gamma_fnc(4)  # => returns 6
+# p gamma_fnc(8)  # => returns 5040
+
+def ice_cream_shop(flavors, favorite)
+    return false if flavors.length == 0
+    return true if flavors.shift == favorite
+
+    ice_cream_shop(flavors, favorite)
+end
+
+# p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+# p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+# p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+# p ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+# p ice_cream_shop([], 'honey lavender')  # => returns false
+
+def reverse(str)
+    return "" if str.length == 0
+
+    reversed = reverse(str[1..-1]) + str[0]
+    reversed
+end
+
+p reverse("house") # => "esuoh"
+p reverse("dog") # => "god"
+p reverse("atom") # => "mota"
+p reverse("q") # => "q"
+p reverse("id") # => "di"
+p reverse("") # => ""
